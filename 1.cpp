e9fb1c830e61289f37dd8c4f24ad77009adc8c60
Kirti Sharma <128890117+KirtiSharma0@users.noreply.github.com>
2025-02-15 20:57:04 +0530

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,16 @@
+//check divisiblity
+
+#include<iostream>
+using namespace std;
+int main(){
+int n;
+cout<<"Enter value of n";
+cin>>n;
+if(n%5==0){
+    cout<<"divisible by 5";
+}
+else{
+    cout<<"not divisible";
+}
+ return 0 ;
+}
